aa3e19366972b79fc3011211a63a9dea0e900f48
Josh Schlesinger <josh@jschlesinger.net>
2013-11-17 14:09:39 -0600

    added missing test helper

---
+++ DefiantCode.WebApiProfiler.Tests/Helpers/FakeDelegatingHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DefiantCode.WebApiProfiler.Tests.Helpers
+{
+    class FakeDelegatingHandler : DelegatingHandler
+    {
+        public HttpResponseMessage Message { get; set; }
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (Message == null)
+            {
+                return base.SendAsync(request, cancellationToken);
+            }
+            return Task.Factory.StartNew(() => Message);
+        }
+
+    }
+}
